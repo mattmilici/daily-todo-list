fcf7e8efd61709ace2fb3477b82fa5037bf1970b
Matthew Milici <milici.m915@gmail.com>
2020-07-31 09:06:21 -0700

    Lock System is working

--- script.js
+++ script.js
@@ -1,71 +1,63 @@
 $(document).ready(function() {
-    startTimer()
+    startTimer();
 
     var lockStatus = false;
     var hourOfDay;
 
+    $(".hourBar").on("click", checkLockStatus);
 
-    $(".hourBar").on("click", checkLockStatus)
-
-
+    // ------------------------------------------Gets the current date------------------------------------------
     $(document).ready(function() {
-        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-        var date = new Date();
-
-        var currentDay = weekDays[date.getDay()]
-        var currentMonth = months[date.getMonth()]
-        var currentDayOfMonth = date.getDate()
-        $("#currentDay").text(currentDay + ", " + currentDayOfMonth + " " + currentMonth)
+        let date = moment().format("dddd, Do MMMM");
+        $("#currentDay").text(date);
     });
 
-
+    // -----------------Gets the hour of the day. This is used to see if the hour has passed and color should be marked gray-------------------------
     function getHourOfDay() {
         let date = new Date();
         hourOfDay = date.getHours();
         return hourOfDay;
     }
-
-
-
+    // -----------------function that checks if the hour has passed. This passing in getHourOfDay function -------------------------
     function hasTheHourPassedCheck() {
-        let currentHour = getHourOfDay()
+        let currentHour = getHourOfDay();
         for (i = 8; i < 18; i++) {
-            if (currentHour >= i) {
-                $("#hour" + i).css("background-color", "#838383")
+            if (currentHour > i) {
+                $("#hour" + i).css("background-color", "#838383");
+            } else if (currentHour === i) {
+                $("#hour" + i).css("background-color", "lightblue");
             } else {
-                $("#hour" + i).css("background-color", "#f3f3f3")
+                $("#hour" + i).css("background-color", "#b6eb7a");
             }
         }
     }
-
-
+    // -----------------function that checks if the hour has passed. This passing in hastheHourPassedCheck function -------------------------
     function startTimer() {
         timeFunc = setInterval(function() {
-            hasTheHourPassedCheck()
-        }, 1);
+            hasTheHourPassedCheck();
+        }, 100);
     }
-
-
+    // -----------------function that checks if the button is locked and chances lockStatus to true or false -------------------------
     function checkLockStatus(event) {
-        event.stopPropagation()
+        // event.stopPropagation();
 
-        let lockIcon = this.className
-        let lockPosition = lockIcon.includes("fas fa-lock-open")
+        let lockIcon = this.className;
+        let lockPosition = lockIcon.includes("fas fa-lock-open");
 
         if (lockPosition) {
-            $(this).removeClass("fas fa-lock-open")
-            $(this).addClass("fas fa-lock")
-            lockStatus = false
+            $(this).removeClass("fas fa-lock-open");
+            $(this).addClass("fas fa-lock");
+            lockStatus = false;
+            console.log(lockStatus);
+            $(this).parent().find("input").attr("disabled", true);
         } else {
-            $(this).removeClass("fas fa-lock")
-            $(this).addClass("fas fa-lock-open")
-            lockStatus = true
+            $(this).removeClass("fas fa-lock");
+            $(this).addClass("fas fa-lock-open");
+            lockStatus = true;
+            $(this).parent().find("input").attr("disabled", false);
         }
         return lockStatus;
     }
 
-
-
+    // -----------------Function determines if a box should be locked or not-------------------------
 });
